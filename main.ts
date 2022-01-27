d9d54f93e221dd1e734642a0fde9608e4c7dac1e
rolo03 <96190771+rolo03@users.noreply.github.com>
2022-01-27 08:55:58 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -86,5 +86,28 @@ let player1 = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-plate1.setPosition(16, 19)
-controller.player1.moveSprite(player1)
+plate1.setPosition(20, 21)
+player1.setPosition(119, 53)
+controller.player1.moveSprite(player1, 100, 100)
+let player2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . b b b b b 5 5 5 5 5 5 5 b . . 
+    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+    . . b d 5 5 b 1 f f 5 4 4 c . . 
+    b b d b 5 5 5 d f b 4 4 4 4 4 b 
+    b d d c d 5 5 b 5 4 4 4 4 4 b . 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+player2.setPosition(26, 57)
+controller.player2.moveSprite(player2, 100, 100)
+player1.setStayInScreen(true)
+player2.setStayInScreen(true)
